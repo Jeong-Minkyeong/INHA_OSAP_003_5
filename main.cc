5355ad4fa86131fcaf3eaed62261fc2ac95ef7bf
minkyeong <stl9955@naver.com>
2024-12-07 11:05:15 +0900

    fix : main 동작 수정

--- main.cc
+++ main.cc
@@ -1,73 +1,89 @@
-#include "avl_tree.h"
 #include <iostream>
 #include <string>
+#include "avl_tree.h"
 
 using namespace std;
 
 void UserTest();
 
 int main() {
-  int T;
-  cin >> T;
-  for (int test_case = 1; test_case <= T; test_case++) {
-    UserTest();
-  }
+   int T;
+   std::cin >> T;
+   for (int test_case = 1; test_case <= T; test_case++)
+   {
+      UserTest();
+   }
 }
 
 void UserTest() {
-  AvlTree<int> avltree;
-  int Q;
-  cin >> Q;
-  for (int i = 0; i < Q; i++) {
-    string command;
-    cin >> command;
+   AvlTree<int> avltree;
+   int Q;
+   std::cin >> Q;
+   
+   for (int i = 0; i < Q; i++)
+   {
+      string command;
+      std::cin >> command;
 
-    if (command == "Insert") {
-      int x;
-      cin >> x;
-      cout << avltree.Insert(x) << "\n";
-    }
+      if (command == "Insert") {
+         int x;
+         std::cin >> x;
+         int result = avltree.Insert(x);
+         std::cout << result << "\n";
+      }
 
-    else if (command == "Erase") {
-      int x;
-      cin >> x;
-      cout << avltree.Erase(x) << "\n";
-    }
+      else if (command == "Empty") {
+         std::cout << avltree.Empty() << "\n";
+      }
 
-    else if (command == "Empty") {
-      avltree.Empty();
-    }
+      else if (command == "Find") {
+         int x;
+         std::cin >> x;
+         std::pair<Node<int>*, int> result = avltree.Find(x);
+         std::cout << result.second  << "\n";
+      }
 
-    else if (command == "Find") {
-      int x;
-      cin >> x;
-      cout << avltree.Find(x) << "\n";
-    }
+      else if (command == "Size") {
+         std::cout << avltree.Size() << "\n";
+      }
 
-    else if (command == "Size") {
-      avltree.Size();
-    }
+      else if (command == "Rank") {
+         int x;
+         std::cin >> x;
+         auto result = avltree.Rank(x);
+         if (result.first == 0) {
+             std::cout << "0\n";
+         } else {
+             std::cout << result.first << " " << result.second << "\n";
+         }
+      }
 
-   /*
-    else if (command == "Rank") {
-      int x;
-      cin >> x;
-      cout << avltree.Rank(x) << "\n";
-    }
- */
-    
-    else if (command == "Average") {
-            int x;
-            cin >> x;
-            cout << avltree.Average(x) << "\n";
-    }
-    
-    
-    else if (command == "Ancestor") {
-            int x;
-            cin >> x;
-            cout << avltree.Ancestor(x).first << " " << avltree.Ancestor(x).second << "\n";
-    }
-    
-  }
+      else if (command == "Erase") {
+         int x;
+         std::cin >> x;
+         int result = avltree.Erase(x);
+         std::cout << result << "\n";
+      }
+
+      else if (command == "Height") {
+         std::cout << avltree.Height() << "\n";
+      }
+
+      else if (command == "Ancestor") {
+         int x;
+         cin >> x;
+         std::pair<int, int> result = avltree.Ancestor(x);
+         std::cout << result.first << ' ' << result.second << "\n";
+      }
+
+      else if (command == "Average") {
+         int x;
+         cin >> x;
+         cout << avltree.Average(x) << "\n";
+      }
+
+      else {
+         cout << "Unknown command: " << command << "\n";
+      }
+   }
 }
