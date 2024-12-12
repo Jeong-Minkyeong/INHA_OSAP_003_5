7db699c56387e83cd98277e4006a4953dd534fbd
minjaehan <tommy3942@naver.com>
2024-12-12 16:18:30 +0900

    docs : 작성자&작성일자 형식 변경

--- node.h
+++ node.h
@@ -1,7 +1,7 @@
 /**
  * MIT License
-
-Copyright (c) 2024-11-26 오픈소스응용프로그래밍 3분반 5팀
+ 
+Copyright (c) 2024 오픈소스응용프로그래밍 3분반 5팀
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,9 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+Written by : 오픈소스응용프로그래밍 3분반 5팀
+Date : 2024-11-26
 */
 
 #ifndef NODE_H_
