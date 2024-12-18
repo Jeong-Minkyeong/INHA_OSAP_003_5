a89c041fc24077e683171b4c679e2dc0dce3d468
kwonte0419 <kwonte0419@naver.com>
2024-12-18 17:17:12 +0900

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kwonte/GoogleTestPrep/opensourcever
-# Build directory: /home/kwonte/GoogleTestPrep/opensourcever/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AvlTreeTest "/home/kwonte/GoogleTestPrep/opensourcever/build/AvlTreeTest")
-set_tests_properties(AvlTreeTest PROPERTIES  _BACKTRACE_TRIPLES "/home/kwonte/GoogleTestPrep/opensourcever/CMakeLists.txt;26;add_test;/home/kwonte/GoogleTestPrep/opensourcever/CMakeLists.txt;0;")
